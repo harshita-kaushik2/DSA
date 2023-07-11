a122d869fa4c53c9e648f3269eb6c405dcce3366
Harshita Kaushik <harshitakaushik.hk@gmail.com>
2023-07-11 19:57:13 +0530

    Update Frog Jump.cpp

--- Dynamic Programming/DSA Sheet/Frog Jump.cpp
+++ Dynamic Programming/DSA Sheet/Frog Jump.cpp
@@ -1,6 +1,7 @@
 https://www.codingninjas.com/studio/problems/frog-jump_3621012
 /*
-Given a number of stairs and a frog, the frog wants to climb from the 0th stair to the (N-1)th stair. At a time the frog can climb either one or two steps. A height[N] array is also given. Whenever the frog jumps from a stair i to stair j, the energy consumed in the jump is abs(height[i]- height[j])
+Given a number of stairs and a frog, the frog wants to climb from the 0th stair to the (N-1)th stair. At a time the frog can climb either one or two steps. 
+A height[N] array is also given. Whenever the frog jumps from a stair i to stair j, the energy consumed in the jump is abs(height[i]- height[j])
 Return the minimum energy that can be used by the frog to jump from stair 0 to stair N-1.
 */
 
@@ -17,7 +18,6 @@ int solve(int ind, vector<int>& height, vector<int>& dp){
     return dp[ind]=min(jumpOne, jumpTwo);
 }
 int main() {
-
   vector<int> height{30,10,60 , 10 , 60 , 50};
   int n=height.size();
   vector<int> dp(n,-1);
@@ -43,7 +43,7 @@ int main() {
   cout<<dp[n-1];
 }
 
-//Tabulation -> Spae Optimization 
+//Tabulation -> Space Optimization 
 
 int main() {
   vector<int> height{30,10,60,10,60,50};
