59891fd1259e37cf4e85ddb400a41b2bea7485ba
Harshita Kaushik <harshitakaushik.hk@gmail.com>
2023-10-14 08:20:59 +0530

    updates

---
+++ Binary Trees/Level Order Traversal.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root == NULL)    return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> row;
+            for(int i=0; i<size; i++){
+                TreeNode* node = q.front();
+                q.pop();
+                if(node->left != NULL)  q.push(node->left);
+                if(node->right != NULL) q.push(node->right);
+                row.push_back(node->val);
+            }
+            ans.push_back(row);
+        }
+        return ans;
+    }
+};
